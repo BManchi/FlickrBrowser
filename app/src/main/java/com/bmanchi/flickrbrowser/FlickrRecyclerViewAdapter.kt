eab85d685dd9c59791ed58e49229e548144da2ee
Bruno <b.manchinelli@gmail.com>
2021-01-15 19:24:59 -0300

    Progress exercise

---
+++ app/src/main/java/com/bmanchi/flickrbrowser/FlickrRecyclerViewAdapter.kt
@@ -0,0 +1,93 @@
+package com.bmanchi.flickrbrowser
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "FlickrReclerViewAdapt"
+
+class FlickrImageViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    var thumbnail: ImageView = view.findViewById(R.id.thumbnail)
+    var title: TextView = view.findViewById(R.id.title)
+}
+class FlickrRecyclerViewAdapter(private var photoList: List<Photo>):RecyclerView.Adapter<FlickrImageViewHolder>() {
+    /**
+     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
+     * an item.
+     *
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     *
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * [.onBindViewHolder]. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary [View.findViewById] calls.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see .getItemViewType
+     * @see .onBindViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlickrImageViewHolder {
+        // Called by the layoutmanager when it needs a new view
+
+        Log.d(TAG, ".onCreateViewHolder new view requested")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.browse, parent, false)
+        return FlickrImageViewHolder(view)
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
+     * position.
+     *
+     *
+     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the `position` parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
+     * have the updated adapter position.
+     *
+     * Override [.onBindViewHolder] instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    override fun onBindViewHolder(holder: FlickrImageViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    override fun getItemCount(): Int {
+        Log.d(TAG, "getItemCount called")
+        return if (photoList.isNotEmpty()) photoList.size else 0
+    }
+
+    fun loadNewData(newPhotos: List<Photo>){
+        photoList = newPhotos
+        notifyDataSetChanged()
+    }
+
+    fun getPhoto(position: Int): Photo? {
+        return if (photoList.isNotEmpty()) photoList[position] else null
+    }
+
+}
---
+++ app/src/main/java/com/bmanchi/flickrbrowser/PhotoDetailsActivity.kt
@@ -0,0 +1,20 @@
+package com.bmanchi.flickrbrowser
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class PhotoDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_details)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
